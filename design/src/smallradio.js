75e6655760e1254d3450af45ef6a7766d01511d3
Khairi Reda <lio889@gmail.com>
2018-07-11 22:33:33 -0500

    UI work

---
+++ design/src/smallradio.js
@@ -0,0 +1,57 @@
+function SmallRadio(g, choices, _callback)
+{
+	this.g = g;
+
+	// clear if anything
+	this.g.selectAll('g').remove();
+
+	// create groups for buttons
+	this.buttons = this.g.selectAll('g').data(choices);
+	this.buttons = this.buttons.enter().append('g').enter()
+		.append('g')
+		.attr('transform', function(d, i) {
+			return 'translate(' + 0 + ',' + (i*14) + ')';
+		})	
+		.merge(this.buttons);
+
+	(function(g, buttons, radio) {
+		buttons.each(function(d, i) 
+		{
+			var thisG = d3.select(this);
+			thisG.append('rect')
+				.attr('x', 0).attr('y', 0).attr('class', 'smallButton' + (i==0 ? ' smallButtonClicked' : ''))
+				.attr('width', '10').attr('height', '10');
+
+			thisG.append('text')
+				.attr('x', -5).attr('y', 9).attr('class', 'smallText')
+				.attr('text-anchor', 'end').html(d.choice);
+
+			thisG.on('click', function(d) 
+			{
+				g.selectAll('rect.smallButton').attr('class', 'smallButton');
+				d3.select(this).select('rect').attr('class', 'smallButton smallButtonClicked');
+				radio.fireCallback(d.choice)
+			});
+		});
+	})(this.g, this.buttons, this);
+
+	if (_callback) {
+		this.callback = _callback;
+	}
+}
+
+SmallRadio.prototype.makeActive = function(_choice) {
+	this.buttons.each(function(d, i) 
+	{
+		d3.select(this).select('rect')
+			.attr('class', 'smallButton ' + (d.choice==_choice ? ' smallButtonClicked' : ''));
+	});
+}
+
+SmallRadio.prototype.addCallback = function() {
+	this.callback = callback;
+}
+
+SmallRadio.prototype.fireCallback = function(choice) {
+	this.callback(choice)
+}
